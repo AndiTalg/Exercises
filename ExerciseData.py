d7b59dd250f9861f8531b0a2da865ae77be3c719
AndiTalg <andreas@familie-talg.de>
2019-05-27 08:07:59 +0200

    Version from iPad, Json, Group class

--- ExerciseData.py
+++ ExerciseData.py
@@ -1,8 +1,13 @@
-import csv
 import json
-from pprint import pprint
 
+class Group:
+    def __init__(self, g):
+        self.name = g["name"]
+        self.image = g["name"] + ".png"
 
+    def __str__(self):
+        return f"Group name: {self.name}, group image: {self.image}"
+        
 class Exercise:
     def __init__(self, ex):
         self.group = ex["group"]
@@ -27,52 +32,48 @@ class Exercises:
         with open(json_path) as f:
             self._ex_dict = json.load(f)
         # Create group list
-        self.group_list = [
-            {"name": g["name"], "image": g["image"]} for g in self._ex_dict["groups"]
-        ]
+        self.group_list = [Group(g) for g in self._ex_dict["groups"]]
         # Create exercise list
         self.ex_list = [Exercise(ex) for ex in self._ex_dict["exercises"]]
 
-    # Dump all exercises to console
-    def dump_list(self):
+    # Dump all exercises to console, only for testing purposes
+    def _dump_exercises(self):
         for ex in self.ex_list:
             print(ex)
+            
+    # Dump all groups to console, only for testing purposes
+    def _dump_groups(self):
+        for g in self.group_list:
+            print(g)
 
-    # # Get all different group-names for first level navigation
+    # Get all different group names for first level navigation
     def group_names(self):
-        return [g["name"] for g in self.group_list]
-
-    # Get all exercises of a specific group
+        return [g.name for g in self.group_list]
+        
+    # Get all exercises of a specific group for second level navigation
     def exercises_of_group(self, group):
         return [ex.name for ex in self.ex_list if ex.group == group]
+    
+    # Get name of group image
+    def group_image(self, name):
+        for g in self.group_list:
+            if g.name == name:
+                return g.image
+                
+    # Get name of exercise image
+    def exercise_image(self, name):
+        for e in self.ex_list:
+            if e.name == name:
+                return e.image
+                
+    # Get group object with given name
+    def get_group(self, name):
+        for g in self.group_list:
+            if g.name == name:
+                return g.image
 
-    # Get exercise object for given exercise name
+    # Get exercise object with given name
     def get_exercise(self, name):
         for ex in self.ex_list:
             if ex.name == name:
                 return ex
-
-
-# myEx = Exercise("barbell", "bench")
-
-# print(myEx)
-
-# print(myEx.name)
-
-# print(myEx.factor)
-
-# myExList = Exercises("exercises.json")
-
-# for g in myExList.group_list:
-#     print(g["name"], g["image"])
-
-# myExList.dump_list()
-# print(myExList.get_exercise("barbell_press"))
-
-# print(myExList.exercises_of_group("bench"))
-# print(myExList.group_names())
-
-# print(myExList.get_exercise('barbell_press'))
-# myExList.dump_list()
-# print(myExList.groups())
-# print(myExList.exercises_of_group("pullup"))
